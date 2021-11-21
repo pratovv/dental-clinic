7fa19142e390d3a8748c3f6146267e7f9cced8e9
Timur_Pratov <tim17112003@gmail.com>
2021-11-21 15:10:26 +0600

    hzhz

--- src/dental-clinic/interfaces/patient.interface.ts
+++ src/dental-clinic/interfaces/patient.interface.ts
@@ -2,6 +2,6 @@ export default interface IPatient{
     readonly patientId:number;
     readonly patientName:string;
     readonly dentistName:'Adilet'|'Feruza';
-    readonly dateA:Date|string;
-    readonly dateB?:Date;
+    readonly dateA:Date|string|boolean;
+    readonly dateB?:Date|boolean;
 }
